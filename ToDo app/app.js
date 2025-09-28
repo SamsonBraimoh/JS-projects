623c756c6fa53e60ceda4459d30869a706957b41
Samson <samsonbraimoh32@gmail.com>
2025-09-29 00:48:06 +0100

    some basic javascript code written

--- ToDo app/app.js
+++ ToDo app/app.js
@@ -1,1 +1,19 @@
-document.getElementsByClassName
+const input = document.getElementById('inputField');
+const Addbutton = document.getElementById('Addbtn');
+const list = document.getElementById('list');
+
+Addbutton.addEventListener("click", ()=> {
+    const task = input.value.trim();
+    if(task !== ""){
+        const li = document.createElement("li");
+        li.textContent = task;
+        const deleteTask = document.createElement("button");
+        deleteTask.textContent = "del";
+        deleteTask.onclick = ()=> li.remove();
+
+        li.appendChild(deleteTask);
+        list.appendChild(li)
+       
+        input.value = "";
+    };
+});
