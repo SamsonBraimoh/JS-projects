14a82d929c95b73692ffe3ce09fe04d81d68140b
Samson <samsonbraimoh32@gmail.com>
2025-10-11 15:45:03 +0100

    digital clock working.

---
+++ Digital clock/app.js
@@ -0,0 +1,25 @@
+// let hour = document.getElementById("hour");
+// let minute = document.getElementById("min");
+// let seconds = document.getElementById("secs");
+// let todayDate = document.getElementById("date")
+// const date = new Date();
+
+// hour.textContent = date.getHours();
+// minute.textContent = date.getMinutes();
+// seconds.textContent = date.getSeconds();
+
+// todayDate.textContent = new Date().toLocaleDateString();
+ 
+const updateClock = () => {
+    const clock = document.getElementById("clock")
+    const time = new Date();
+
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let seconds = time.getSeconds();
+
+    clock.textContent = `${hour}:${minute}:${seconds}`;
+
+} 
+setInterval(updateClock, 1000);
+updateClock();
